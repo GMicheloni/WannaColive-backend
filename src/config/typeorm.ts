9e0598bbbbd25d95159edbc762bc2257e3dcb379
Gian <gmicheloni52@gmail.com>
2025-11-24 09:06:28 -0300

    fix demo3

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,33 +5,25 @@ const envFilePath =
   process.env.NODE_ENV === 'production'
     ? '.env.production'
     : '.env.development';
-
 dotenv.config({ path: envFilePath });
-
-const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
   type: 'postgres',
-
-  // ⬇️ Si usas una URL (Render/Neon/ElephantSQL), TypeORM usará esto
-  url: process.env.DB_URL || undefined,
-
-  // ⬇️ Solo si NO usas una URL
+  url: process.env.DB_URL,
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT!,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
-
   autoLoadEntities: true,
   synchronize: true,
-
-  ssl: isProd
-    ? { rejectUnauthorized: false } // Render exige esto
-    : false,
+  dropSchema: false,
+  // ⚠️ solo en desarrollo - elimina y recrea el esquema cada vez que se inicia la app
+  ssl: {
+    rejectUnauthorized: false, // importante para Render
+  },
 };
 
 export default registerAs('typeorm', () => config);
